0bdff94c6fd45d6e06d51803f2dbfedc73581f8e
Bharadwaj-code <svaranasi1997@gmail.com>
2022-06-24 01:10:24 +0800

    function working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
-function App() {
-  return (
-    <div className="app">
-      <Header />
-    </div>
-  );
+class App extends Component() {
+  sayHello(name) {
+    return `hello ${name}`;
+  }
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <h1>{this.sayHello("sunny")}</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
