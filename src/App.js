feb03f29614b594d5a1769d5b782216893787b67
Bharadwaj-code <svaranasi1997@gmail.com>
2022-06-23 23:49:09 +0800

    its working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
 
+import "./App.css";
+import Header from './components/Header';
 function App() {
   return (
     <div className="app">
       <Header />
-    </div>
-  );
-}
-function Header() {
-  return (
-    <div className="header">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Intro />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
