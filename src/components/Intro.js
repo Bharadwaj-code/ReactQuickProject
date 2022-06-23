c4cdc24866ebb8c02976389cd6a26fb3c2ba6506
Bharadwaj-code <svaranasi1997@gmail.com>
2022-06-23 23:53:55 +0800

    fixed the formatting

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,11 @@
 function Intro() {
-    return (
-      <div className="intro">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div className="intro">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
 
-  export default Intro;
+export default Intro;
